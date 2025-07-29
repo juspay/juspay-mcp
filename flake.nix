599938b886c6a206157b1fd4ffbdce3b96b01b1e
Nikhil Singh <nik.singh710@gmail.com>
2025-07-29 14:48:16 +0530

    refactor: Migrate to `python-flake`

--- flake.nix
+++ flake.nix
@@ -9,19 +9,9 @@
     git-hooks.url = "github:cachix/git-hooks.nix";
     git-hooks.flake = false;
 
-    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
-    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    uv2nix.url = "github:pyproject-nix/uv2nix";
-    uv2nix.inputs.pyproject-nix.follows = "pyproject-nix";
-    uv2nix.inputs.nixpkgs.follows = "nixpkgs";
-
     nix2container.url = "github:nlewo/nix2container";
 
-    pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs";
-    pyproject-build-systems.inputs.pyproject-nix.follows = "pyproject-nix";
-    pyproject-build-systems.inputs.uv2nix.follows = "uv2nix";
-    pyproject-build-systems.inputs.nixpkgs.follows = "nixpkgs";
+    python-flake.url = "github:juspay/python-flake/pull/2/head";
   };
 
   outputs = inputs@{ flake-parts, ... }:
