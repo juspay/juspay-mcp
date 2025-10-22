1592adc9ac9c218dabf080c38bbe6372b8a92412
nishant.katkade <nishant.katkade@juspay.in>
2025-10-22 20:42:37 +0530

    payment methods hardcoded in list offers

--- juspay_mcp/api/offer.py
+++ juspay_mcp/api/offer.py
@@ -40,11 +40,18 @@ async def list_offers_juspay(payload: dict, meta_info: dict = None) -> dict:
     if "amount" in payload["order"] and not isinstance(payload["order"]["amount"], str):
         payload["order"]["amount"] = str(payload["order"]["amount"])
     
-    payload["payment_method_info"] = [{
-        "payment_method_type": "CASH",
-        "payment_method": "CASH", 
-        "payment_method_reference": "CASH_CASH_ref"
-    }]
+    payload["payment_method_info"] = [
+        {
+            "payment_method_type": "CASH",
+            "payment_method": "CASH", 
+            "payment_method_reference": "CASH_CASH_ref"
+        },
+        {
+            "payment_method_type": "CARD",
+            "payment_method": "CARD", 
+            "payment_method_reference": "CARD_CARD_ref"
+        }
+    ]
     
     routing_id = payload.get("routing_id")
     if "routing_id" in payload:
